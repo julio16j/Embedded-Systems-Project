5613623e7686adcbb300d25e54bf9b8c1e5adb75
Julio Cesar Urbano <jc.urbano.vasconcelos@gmail.com>
2022-11-15 19:41:33 -0300

    remove old api and setup docs from the new one

--- digital-lock-api/src/config/database_configuration.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-SQL_LITE_URL = "sqlite:///project.db"
-
-def setup_database (app_instance):
-    database = SQLAlchemy()
-    database.init_app(app_instance)
-    return database
--- digital-lock-api/src/model/entity/user.py
+++
@@ -1,6 +0,0 @@
-from src.app import database
-
-class User(database.Model):
-    key = database.Column(database.String, primary_key=True)
-    username = database.Column(database.String,nullable=False)
-    
